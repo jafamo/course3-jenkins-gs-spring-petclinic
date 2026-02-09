772ddeadc648b16bf28d277cc8e840fc69915347
Javier <jafamo@gmail.com>
2026-02-09 13:39:08 +0100

    Update VetTests.java
    pruebas

--- src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
@@ -33,6 +33,7 @@ class VetTests {
 		vet.setId(123);
 		Vet other = (Vet) SerializationUtils.deserialize(SerializationUtils.serialize(vet));
 		assertThat(other.getFirstName()).isEqualTo(vet.getFirstName());
+		assertThat(other.getFirstName()).isEqualTo("foo");
 		assertThat(other.getLastName()).isEqualTo(vet.getLastName());
 		assertThat(other.getId()).isEqualTo(vet.getId());
 	}
